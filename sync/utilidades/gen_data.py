4f1aa1cd3a1c8de250baa2982fd650429fea886f
Tulio Ruiz <lucipher.80@gmail.com>
2013-01-18 15:33:11 -0430

    correccion en las consultas cuando la tabla cliente esta vacia

--- sync/utilidades/gen_data.py
+++ sync/utilidades/gen_data.py
@@ -16,7 +16,7 @@ cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
 
 start_date = date(2013, 1, 1)
 day = timedelta(days = 1)
-n_products = 3
+n_products = 300
 n_clients = 2
 
 for i in xrange(0, n_products):
@@ -25,31 +25,31 @@ for i in xrange(0, n_products):
 conn.commit()
 print "Se agregaron los productos..."
 
-for i in xrange(0, n_clients):
-    cur.execute("insert into clients (id, name, address, phone, vat) values(%s, %s, %s, %s, %s)",
-                (str(uuid.uuid4()), "Cliente %s"%i, "Direccion %s"%i, str(random.randint(20000, 100000)), str(random.randint(10000000, 100000000))))
-conn.commit()
-print "Se agregaron los clientes..."
+# for i in xrange(0, n_clients):
+#     cur.execute("insert into clients (id, name, address, phone, vat) values(%s, %s, %s, %s, %s)",
+#                 (str(uuid.uuid4()), "Cliente %s"%i, "Direccion %s"%i, str(random.randint(20000, 100000)), str(random.randint(10000000, 100000000))))
+# conn.commit()
+# print "Se agregaron los clientes..."
 
-cur.execute("select id from clients")
-all_clients = cur.fetchall()
-cur.execute("select id from products")
-all_products = cur.fetchall()
-conn.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT)
+# cur.execute("select id from clients")
+# all_clients = cur.fetchall()
+# cur.execute("select id from products")
+# all_products = cur.fetchall()
+# conn.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT)
 
-for i in xrange(0, n_clients):
-    client = all_clients[random.randint(0, len(all_clients)-1)]
-    sale_id = cur.execute("insert into sales (id, client_id, sale_date) values(%s, %s, %s) returning id",
-                (str(uuid.uuid4()), client[0], start_date, ))
-    sale_id = cur.fetchone()[0]
-    print "Generando venta ", sale_id
-    n_products_sale = random.randint(1, 30)
-    for j in xrange(n_products_sale):
-        product_id = all_products[random.randint(0, len(all_products)-1)][0]
-        cur.execute("insert into sales_lines (id, sale_id, product_id, quantity) values(%s, %s, %s, %s)",
-                    (str(uuid.uuid4()), sale_id, product_id, random.randint(1, 10)))
-    conn.commit()
-    if random.randint(0, 20) > 10:
-        start_date = start_date + day
-conn.commit()
-print "Todas las ventas generadas."
+# for i in xrange(0, n_clients):
+#     client = all_clients[random.randint(0, len(all_clients)-1)]
+#     sale_id = cur.execute("insert into sales (id, client_id, sale_date) values(%s, %s, %s) returning id",
+#                 (str(uuid.uuid4()), client[0], start_date, ))
+#     sale_id = cur.fetchone()[0]
+#     print "Generando venta ", sale_id
+#     n_products_sale = random.randint(1, 30)
+#     for j in xrange(n_products_sale):
+#         product_id = all_products[random.randint(0, len(all_products)-1)][0]
+#         cur.execute("insert into sales_lines (id, sale_id, product_id, quantity) values(%s, %s, %s, %s)",
+#                     (str(uuid.uuid4()), sale_id, product_id, random.randint(1, 10)))
+#     conn.commit()
+#     if random.randint(0, 20) > 10:
+#         start_date = start_date + day
+# conn.commit()
+# print "Todas las ventas generadas."
